35dd2a1b050300afb6daf49e9750ff74b6f9588a
lyk625358 <40014717+lyk625358@users.noreply.github.com>
2018-09-26 13:19:06 +0800

    Update 32for.php
    格式好看了！

--- 32for.php
+++ 32for.php
@@ -6,4 +6,8 @@
  * @Last Modified by:   lyk625358@163.com
  * @Last Modified time: 2018-09-26 11:01:40
  */
-	for($i=1;$i<3;$i++){for($j=1;$j<4;$j++){echo $j;}}
+	for($i=1;$i<3;$i++){
+		for($j=1;$j<4;$j++){
+			echo $j;
+		}
+	}
