3344bbab9774eae1fe9d656c5c31e80532a99281
lyk625358 <lyk625358@163.com>
2018-10-09 11:50:27 +0800

    kkkkkkkkkkk

---
+++ boolean.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Author: lyk625358@163.com
+ * @Date:   2018-10-09 09:14:10
+ * @Last Modified by:   lyk625358@163.com
+ * @Last Modified time: 2018-10-09 09:16:50
+ */
+	$arr = array();
+	if(!$arr){
+		echo "no";
+	}
