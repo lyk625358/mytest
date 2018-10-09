b4db940188acd791a185c2ed9f0f4306fefe393c
lyk625358 <40014717+lyk625358@users.noreply.github.com>
2018-10-09 14:05:52 +0800

    新建文件array_filter.php

---
+++ array_filter.php
@@ -0,0 +1,13 @@
+<?php
+	$array1 = array("a"=>1, "b"=>2, "c"=>3, "d"=>4, "e"=>5);
+	$array2 = array(6, 7, 8, 9, 10, 11, 12);
+
+	echo "Odd :\n";
+	print_r(array_filter($array1, function ($var){
+	    return($var & 1);
+	}));
+
+	echo "Even:\n";
+	print_r(array_filter($array2, function ($var){
+	    return(!($var & 1));
+	}));
