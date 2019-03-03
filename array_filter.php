a12acc20072b6fd269251cc856ae2434aa71f9a4
lyk625358 <40014717+lyk625358@users.noreply.github.com>
2019-03-03 12:52:55 +0800

    Update array_filter.php

--- array_filter.php
+++ array_filter.php
@@ -22,7 +22,3 @@
 	//Git is a distributed version control system.
 	//Git is free software.
 	
-	//Git is a distributed version control system.
-	//Git is free software distributed under the GPL.
-	
-	//Creating a new branch is quick.
